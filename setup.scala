be89e1779c63d0bbb665f4830a3fd161215ab53e
daniel.beskin <daniel.beskin@gmail.com>
2024-12-11 19:58:09 +0200

    Bump Scala version

--- setup.scala
+++ setup.scala
@@ -1,2 +1,3 @@
-//> using scala "3.4.1"
-//> using test.dep "org.scalatest::scalatest:3.2.18"
+//> using scala "3.6.2"
+//> using test.dep "org.scalatest::scalatest:3.2.19"
+//> using option -experimental
